3da377b877082aaa080f7f96e1867b69da58f035
Starlight <linjerome95@gmail.com>
2025-06-13 09:50:02 +0200

    plane more triangles && hole

--- Assets/Scripts/Terrains/PlaneGenerator.cs
+++ Assets/Scripts/Terrains/PlaneGenerator.cs
@@ -1,36 +1,106 @@
-
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaneGenerator : MonoBehaviour
 {
-    private float radius = 4.5f;
-    private float depth = 10;
     private Mesh Mesh;
-    private Vector3[] curVerticles;
-    private Vector3[] newVerticles;
+    private MeshFilter MeshFilter;
+    private MeshCollider MeshCollider;
+    [SerializeField] private Vector2 planeSize = new Vector2(1, 1);
+    [SerializeField] private int planeResolution = 1;
 
+    private List<Vector3> verticles;
+    private List<int> triangles;
+    
+    [SerializeField] private float radius = 4.5f;
+    [SerializeField] private float depth = 10;
+
+    
+    
+    
+    
     private void Start()
     {
-        Mesh = GetComponent<MeshFilter>().mesh;
-        curVerticles = Mesh.vertices;
-        newVerticles = new Vector3[curVerticles.Length];
-        Hole();
+        Mesh = new Mesh();
+        MeshFilter = GetComponent<MeshFilter>();
+        MeshFilter.mesh = Mesh;
+        MeshCollider = GetComponent<MeshCollider>();
     }
 
 
-
-    private void Hole()
+    
+    
+    
+    private void Update()
     {
-        for (int i = 0; i < curVerticles.Length; i++)
+        GeneratePlane(planeSize, planeResolution);
+        Hole();
+        AssignMesh();
+    }
+
+
+    
+    
+    
+    private void GeneratePlane(Vector2 size, int resolution)
+    {
+        verticles = new List<Vector3>();
+        float xPerStep = size.x / resolution;
+        float yPerStep = size.y / resolution;
+        for (int y = 0; y < resolution + 1; y++)
         {
-            Vector3 vertex = curVerticles[i];
-            float distance = new Vector2(vertex.x, vertex.z).magnitude;
-            if (distance < radius) vertex.y -= depth;
-            newVerticles[i] = vertex;
+            for (int x = 0; x < resolution+1; x++)
+            {
+                verticles.Add(new Vector3(x * xPerStep - size.x / 2f, 0, y * yPerStep - size.y / 2f));
+            }
         }
 
-        Mesh.vertices = newVerticles;
+        triangles = new List<int>();
+        for (int row = 0; row < resolution; row++)
+        {
+            for (int col = 0; col < resolution; col++)
+            {
+                int i = row * resolution + row + col;
+                triangles.Add(i);
+                triangles.Add(i + resolution + 1);
+                triangles.Add(i + resolution + 2);
+
+                triangles.Add(i);
+                triangles.Add(i + resolution + 2);
+                triangles.Add(i + 1);
+            }
+        }
+    }
+
+    
+    
+    
+
+    private void AssignMesh()
+    {
+        Mesh.Clear();
+        Mesh.vertices = verticles.ToArray();
+        Mesh.triangles = triangles.ToArray();
+        Mesh.RecalculateNormals();
+        Mesh.RecalculateBounds();
+
+        MeshCollider.sharedMesh = null;
+        MeshCollider.sharedMesh = Mesh;
+    }
+    
+    
+    
+    
+    
+    private void Hole()
+    {
+        for (int i = 0; i < verticles.Count; i++)
+        {
+            Vector3 vertex = verticles[i];
+            float distance = new Vector2(vertex.x, vertex.z).magnitude;
+            if (distance < radius) vertex.y -= depth;
+            verticles[i] = vertex;
+        }
     }
 }
