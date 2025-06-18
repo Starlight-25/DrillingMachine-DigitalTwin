962a4727b28ad7e34fc403e3607ef1b92dbdf9ca
Starlight <linjerome95@gmail.com>
2025-06-18 13:25:38 +0200

    post processing fog

--- Assets/Scripts/PostProcessing/PostProcessing.cs
+++ Assets/Scripts/PostProcessing/PostProcessing.cs
@@ -24,5 +24,17 @@ public class PostProcessing : MonoBehaviour
     private void ChangeEffect(bool underwater)
     {
         Volume.profile = underwater ? UnderWaterPostProcessing : SurfacePostProcessing;
+        if (underwater)
+        {
+            RenderSettings.fogColor = new Color(0x56/255f, 0xBF/255f, 0xCF/255f); //56BFCF
+            RenderSettings.fogStartDistance = 25f;
+            RenderSettings.fogEndDistance = 75f;
+        }
+        else
+        {
+            RenderSettings.fogColor = new Color(0xA1/255f, 0xBA/255f, 0xC1/255f); //A1BAC1
+            RenderSettings.fogStartDistance = 500f;
+            RenderSettings.fogEndDistance = 700f;
+        }
     }
 }
