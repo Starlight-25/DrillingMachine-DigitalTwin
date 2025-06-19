7954740bacad83db933383e976e5d733246b9869
Starlight <linjerome95@gmail.com>
2025-06-19 10:01:07 +0200

    fog Settings

--- Assets/Scripts/PostProcessing/PostProcessing.cs
+++ Assets/Scripts/PostProcessing/PostProcessing.cs
@@ -1,38 +1,67 @@
-using System;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class PostProcessing : MonoBehaviour
+public class PostProcessing : MonoBehaviour, ISettingsUpdater
 {
     private Volume Volume;
     [SerializeField] private VolumeProfile SurfacePostProcessing;
     [SerializeField] private VolumeProfile UnderWaterPostProcessing;
     
     private float WaterSurface = 30f;
+    private Color SurfaceFogColor = new(0xA1 / 255f, 0xBA / 255f, 0xC1 / 255f); //A1BAC1
+    private Color UnderwaterFogColor = new(0x56 / 255f, 0xBF / 255f, 0xCF / 255f); //56BFCF
 
+    [SerializeField] private SettingsHandler SettingsHandler;
+    private int FogDistance;
+    
+    
+    
+    
+    
     private void Start()
     {
+        SettingsHandler.Add(this);
+        
         Volume = GetComponent<Volume>();
+        //FogDistance = SettingsHandler.Settings.Graphics.FogDistance;
     }
 
+    
+    
+    
+    
     private void Update()
     {
         float CameraY = transform.position.y;
         ChangeEffect(CameraY >= 0 && CameraY <= WaterSurface);
     }
 
+    
+    
+    
+    
+    public void UpdateFromSettings()
+    {
+        FogDistance = SettingsHandler.Settings.Graphics.FogDistance;
+    }
+
+    
+    
+    
+    
     private void ChangeEffect(bool underwater)
     {
-        Volume.profile = underwater ? UnderWaterPostProcessing : SurfacePostProcessing;
         if (underwater)
         {
-            RenderSettings.fogColor = new Color(0x56/255f, 0xBF/255f, 0xCF/255f); //56BFCF
+            Volume.profile = UnderWaterPostProcessing;
+            RenderSettings.fogColor = UnderwaterFogColor;
             RenderSettings.fogStartDistance = 25f;
-            RenderSettings.fogEndDistance = 75f;
+            RenderSettings.fogEndDistance = SettingsHandler.Settings.Graphics.FogDistance;
         }
         else
         {
-            RenderSettings.fogColor = new Color(0xA1/255f, 0xBA/255f, 0xC1/255f); //A1BAC1
+            Volume.profile = SurfacePostProcessing;
+            RenderSettings.fogColor = SurfaceFogColor;
             RenderSettings.fogStartDistance = 100f;
             RenderSettings.fogEndDistance = 450f;
         }
