b5f1f659dbdea3dc664338445f68faa9f228fbbd
Starlight <linjerome95@gmail.com>
2025-07-04 10:33:49 +0200

    Credits text in Credits Menu

--- Assets/Scripts/UI/MainMenu/CreditsMenuHandler.cs
+++ Assets/Scripts/UI/MainMenu/CreditsMenuHandler.cs
@@ -1,11 +1,14 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
-public class CreditsMenuHandler : MonoBehaviour
+public class CreditsMenuHandler : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private GameObject CreditsMenu;
     [SerializeField] private GameObject MainMenu;
+    [SerializeField] private TextMeshProUGUI CreditsText;
 
     [SerializeField] private PlayerInput MainMenuInput;
     private InputAction ReturnInputAction;
@@ -14,7 +17,11 @@ public class CreditsMenuHandler : MonoBehaviour
     
     
     
-    private void Start() => ReturnInputAction = MainMenuInput.actions["Return"];
+    private void Start()
+    {
+        ReturnInputAction = MainMenuInput.actions["Return"];
+        CreditsText.text = Resources.Load<TextAsset>("credits").text;
+    }
     
     
     
@@ -33,5 +40,20 @@ public class CreditsMenuHandler : MonoBehaviour
     {
         MainMenu.SetActive(true);
         CreditsMenu.SetActive(false);
+    }
+
+
+
+    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(CreditsText, eventData.position, null);
+        if (linkIndex != -1)
+        {
+            TMP_LinkInfo linkInfo = CreditsText.textInfo.linkInfo[linkIndex];
+            string linkId = linkInfo.GetLinkID();
+            Application.OpenURL(linkId);
+        }
     }
 }
