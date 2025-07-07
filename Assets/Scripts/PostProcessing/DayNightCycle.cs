51cd4a9e5eda940611ae165f6acba2c1bc19491e
Starlight <linjerome95@gmail.com>
2025-07-07 16:18:40 +0200

    rotqation light replay mode

--- Assets/Scripts/PostProcessing/DayNightCycle.cs
+++ Assets/Scripts/PostProcessing/DayNightCycle.cs
@@ -5,17 +5,34 @@ public class DayNightCycle : MonoBehaviour
     private Transform Light;
     [SerializeField] private MainUIHandler MainUIHandler;
     
+    
+    
+    
+    
     private void Start()
     {
         Light = transform;
     }
 
+    
+    
+    
 
     private void Update()
     {
         float curTime = MainUIHandler.GetCurTime();
         float timeInHour = curTime / 3600 % 24;
-        float sunAngle = (timeInHour / 24f) * 360f - 90f;
+        
+        LightRotation(timeInHour);
+    }
+
+    
+    
+    
+
+    private void LightRotation(float timeInHour)
+    {
+        float sunAngle = timeInHour / 24f * 360f - 90f;
         Light.rotation = Quaternion.Euler(sunAngle, 0, 0);
     }
 }
---
+++ Assets/Scripts/PostProcessing/ReplayDayNightCycle.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayDayNightCycle : MonoBehaviour
+{
+    private Transform Light;
+    [SerializeField] private DrillingDataManager DrillingDataManager;
+    private List<DrillingDataCSV> DrillingData;
+    
+    private const string dateFormat = "dd/MM/yyyy HH:mm";
+    private DateTime startTime;
+    
+    
+    
+    
+
+    private void Start()
+    {
+        Light = transform;
+        DrillingData = DrillingDataManager.DrillingData;
+
+        startTime = DateTime.ParseExact(DrillingData[0].Date, dateFormat, null);
+    }
+
+    
+    
+    
+    
+    private void Update()
+    {
+        float timeInHour = GetTimeInHour();
+        LightRotation(timeInHour);
+    }
+
+    
+    
+    
+    
+    private float GetTimeInHour()
+    {
+        DateTime curTime = DateTime.ParseExact(DrillingData[DrillingDataManager.Index].Date, dateFormat, null);
+        return (float)(curTime - startTime).TotalHours % 24f;
+    }
+    
+    
+    
+    
+    
+    private void LightRotation(float timeInHour)
+    {
+        float sunAngle = timeInHour / 24f * 360f - 90f;
+        Light.rotation = Quaternion.Euler(sunAngle, 0, 0);
+    }
+
+}
